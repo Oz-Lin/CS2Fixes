ee5975bc84dd0e3c65873f3ac15a76ce4f64328e
Oz_Lin <lin@ozlin.info>
2025-12-31 23:39:27 +1100

    1

--- src/cs2fixes.h
+++ src/cs2fixes.h
@@ -96,7 +96,7 @@ public: // hooks
 	void Hook_ApplyGameSettings(KeyValues* pKV);
 	void Hook_CreateWorkshopMapGroup(const char* name, const CUtlStringList& mapList);
 	void Hook_GoToIntermission(bool bAbortedMatch);
-	bool Hook_OnTakeDamage_Alive(CTakeDamageResult* pDamageResult);
+	//bool Hook_OnTakeDamage_Alive(CTakeDamageResult* pDamageResult);
 	void Hook_PhysicsTouchShuffle(CUtlVector<TouchLinked_t>* pList, bool unknown);
 #ifdef PLATFORM_WINDOWS
 	Vector* Hook_GetEyePosition(Vector*);
